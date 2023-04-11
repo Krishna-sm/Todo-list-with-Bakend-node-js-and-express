6a4d8f6d91b922384b47ab805764544c0662e735
krishna-sm <krishna.web18@gmail.com>
2023-04-11 12:49:42 +0530

    adding swagger ui with crud

--- backend/controllers/todosController.js
+++ backend/controllers/todosController.js
@@ -14,13 +14,18 @@ const createTodo=async(req,res)=>{
 
           const todo = await  TodoModel.create({title,desc});
 
-          return res.status(201).json({success:true,msg:"create successfully",todo})
+          return res.status(201).json({success:200,msg:"create successfully",todo})
 
 }
 
 const allTodo=async(req,res)=>{
+  if(!req.headers['authorization'])
+  {
+    return res.status(401).json({success:401,msg:"Dont Fetch"})
+
+  }
         const Todos = await TodoModel.find({});
-        return res.status(201).json({success:true,msg:"fetch  successfully",Todos})
+        return res.status(201).json({success:200,msg:"fetch  successfully",Todos})
 }
 
 const updateTodo=async(req,res)=>{
@@ -37,7 +42,12 @@ const updateTodo=async(req,res)=>{
                       return  res.status(200).json({success:false,msg:"Desc is reqired"})
                     }
                     const data = await TodoModel.findByIdAndUpdate(id,{title,desc});
-                    res.status(200).json({success:true,msg:"update success"})
+                    if(!data)
+                    {
+                      return  res.status(200).json({success:false,msg:"todo not found"})
+
+                    }
+                    res.status(200).json({success:200,msg:"update success"})
     } catch (error) {
         return res.status(404).json({success:false,msg:"Todo not found"})
     
@@ -49,7 +59,12 @@ const deleteTodo=async(req,res)=>{
         const {id} = req.params;
       
         const data = await TodoModel.findByIdAndDelete(id);
-        res.status(200).json({success:true,msg:"delete success"})
+        if(!data)
+        {
+          return  res.status(200).json({success:false,msg:"todo not found"})
+
+        }
+        res.status(200).json({success:200,msg:"delete success"})
 } catch (error) {
 return res.status(404).json({success:false,msg:"Todo not found"})
 
--- backend/index.js
+++ backend/index.js
@@ -6,6 +6,10 @@ const port = process.env.PORT || 8000;
 const cors = require('cors');
 const morgan = require('morgan');
 const { connectDB } = require('./config/db');
+const swagger = require('swagger-ui-express')
+const YAML = require('yamljs')
+
+const SwaggerJsDoc = YAML.load('./auth.yaml');
 
 // middlwares
 connectDB();
@@ -15,6 +19,12 @@ app.use(cors())
 app.use(morgan('dev'))
 
 app.use('/api/v1',require('./routes/todoRoute'));
+app.use('/',swagger.serve,swagger.setup(SwaggerJsDoc,{
+    customCss: '.swagger-ui .topbar { display: none }',
+}))
+
+
+
 app.listen(port,()=>{
     console.log(`the server is Listen at the http://localhost:${port}`)
 })
